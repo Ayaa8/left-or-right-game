d3c8d43ca99643b867397b2a53eb932ff1139ef9
Ayaa8 <89164655+Ayaa8@users.noreply.github.com>
2021-10-08 14:50:20 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+
+int main(){
+printf("left or right chose the right path with 1 or 2 | 1 is right | 2 is left\n");
+int check;
+scanf("%d",&check);
+if(check == 1){
+printf("you just won\n");
+}
+else{
+printf("good bye loser\n");
+}
+}
+
+
